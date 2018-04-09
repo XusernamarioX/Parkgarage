41261bf641a23b21d509fb8d208447d5143f7bad
XusernamarioX <171wita11@o365.htl-leoben.at>
2018-04-09 16:07:42 +0200

    alles klar

---
+++ src/Parkgaragen.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Parkgaragen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 0; i == 82139; i++) {
+			System.out.println("Nico du Sp‰‰‰‰st");
+		}
+		
+		
+	}
+
+}
